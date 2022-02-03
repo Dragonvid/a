39651d6a6d8486928a049b45c5a2e99c5a43979f
davsan03 <davsan03@ubuntu>
2022-02-03 19:03:55 +0100

    Multiplica dos numeros

---
+++ aa/src/aa/Aa.java
@@ -0,0 +1,24 @@
+package aa;
+
+/**
+ * @author davsan03
+ */
+public class Aa {
+        /**
+     * Método multiplica que multiplica a por b
+     * @param a La primera variable usada en multiplicación
+     * @param b La segunda variable usada en la multiplicación
+     * @return Devuelve el resultado de la operación
+     */
+    public int multiplica(int a, int b){
+        return a*b;
+    }
+        /**
+     * Método main que no hace nada
+     * @param args Parámetros de entrada de línea de comandos
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
