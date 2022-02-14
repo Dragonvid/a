8822621fc0181c4b96a2675e4579e0eda989fd3d
davsan03 <davsan03@ubuntu>
2022-02-14 17:35:56 +0100

    He añadido factorial para realizar el factorial de un número

--- aa/src/aa/Test1.java
+++ aa/src/aa/Test1.java
@@ -13,6 +13,20 @@ public class Test1 {
     public int multiplica(int a, int b){
         return a*b;
     }
+    /**
+     * Método factorial que obtiene el factorial de un numero
+     * @param numero Int del cual se tiene que sacar el factorial
+     * @param i Int contador utilizado en el bucle para sacar el factorial
+     * @param factorial guarda el resultado del factorial
+     * @return Devuelve el resultado de la operación
+     */
+    public int factorial(int numero){
+        int factorial=numero;
+        for(int i=(numero-1);i>1;i--){
+            factorial=factorial*i;
+        }
+        return factorial;
+    }
         /**
      * Método main que no hace nada
      * @param args Parámetros de entrada de línea de comandos
